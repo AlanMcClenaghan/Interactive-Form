10f508e2d1c1ea3d95f30ac46463a5961c39fa18
Alan McClenaghan <AlanM@Alans-MBP-2.broadband>
2024-01-04 21:24:17 +0000

    2. Name Field

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log("Test");
+// console.log("Test");
+
+/* When the page first loads, 
+the first text field should have the focus state by default to prompt the user. 
+*/
+
+// Select Name Field
+const nameField = document.querySelector('#name');
+console.log(nameField);
+
+// Function to set focus
+function setFocus(field) {
+    field.classList.add('focus');
+}
+
+// Call setFocus function on first text field
+setFocus(nameField);
