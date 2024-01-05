46d00ab0ddbe9b00c4556616445e4f35a2ba85bd
Alan McClenaghan <AlanM@Alans-MBP-2.broadband>
2024-01-05 13:00:56 +0000

    4. T-Shirt Info Section

--- js/script.js
+++ js/script.js
@@ -27,22 +27,68 @@ and only be displayed if "Other" is selected in the drop-down menu.
 
 // Select Other Field
 const otherJobRole = document.querySelector('#other-job-role');
-console.log(otherJobRole);
+// console.log(otherJobRole);
 
 // Hide Other Field by default
 otherJobRole.hidden = true;
 
 // Select Job Role Field
 const jobRole = document.querySelector('#title');
-console.log(jobRole);
+// console.log(jobRole);
 
 // Add EventListener to Job Role Field
 jobRole.addEventListener('change', e => {
-    console.log(e.target.value);
+    // console.log(e.target.value);
     // Get value when the changed
     const role = e.target.value;
-    console.log(role);
+    // console.log(role);
 
     // Reveal/Hide Other field depending on value of 
     role === 'other' ? otherJobRole.hidden = false : otherJobRole.hidden = true;
+});
+
+/* 
+The options in the "Color" drop-down menu are not available for each t-shirt design, 
+so the user shouldn’t be able to see or choose a color option until they have chosen a design.
+*/
+
+// Disable the "Color" <select> element.
+
+// Select Color Field
+const color = document.querySelector('#color');
+console.log(color);
+
+// Disable Color by default
+color.disabled = true;
+
+
+// Set up the "Design" <select> element to listen for changes. When a change is detected:
+
+// Select Design Field
+const design = document.querySelector('#design');
+console.log(design);
+
+design.addEventListener('change', e => {
+    console.log(e.target.value);
+    // Get value when the changed
+    const design = e.target.value;
+    console.log(design);
+
+    // The "Color" <select> element should be enabled.
+    color.disabled = false;
+    
+    // The "Color" <select> element should display an available color.
+    // The "Color" dropdown menu should display only the color options associated with the selected design. For example:
+        // If the user selects "Theme - JS Puns" then the "Color" menu should only display "Cornflower Blue," "Dark Slate Grey," and "Gold."
+        // If the user selects "Theme - I ♥ JS" then the "Color" menu should only display "Tomato," "Steel Blue," and "Dim Grey."
+
+    const options = color.children;
+    console.log(options[0]);
+    options[0].textContent = "Select a Color";
+
+    for (let i = 0; i < options.length; i++) {
+        console.log(options[i]);
+        options[i].dataset.theme === design ? options[i].hidden = false : options[i].hidden = true;
+    }
+
 });
